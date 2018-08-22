965bacfd35fe7423bbce66639576af1e0567ef01
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-22 08:24:43 +0800

    set up mongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express'),
+      mongoose = require('mongoose'),
       app = express();
 
 const PORT = process.env.PORT || 5000;
+
+/* DB connection */
+const DB_URL = require('./config/keys').mongoURI;
+mongoose.connect(DB_URL)
+  .then(() => console.log('MongoDB running...'))
+  .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('I AM ALIVE!!!');
