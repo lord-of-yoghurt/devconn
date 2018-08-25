7371eafadd7f5d8bab5e2d43f0ad2a829b8a562f
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-25 17:54:15 +0800

    fill out validations for /register route

--- validations/register.js
+++ validations/register.js
@@ -5,8 +5,42 @@ const isEmpty = require('./is-empty');
 module.exports = function valudateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  // this is from the 'confirm password' field
+  data.confPassword = !isEmpty(data.confPassword) ? data.confPassword : '';
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name cannot be empty';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email cannot be empty';
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be between 6 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password cannot be empty';
+  }
+
+  if (Validator.isEmpty(data.confPassword)) {
+    errors.confPassword = 'Please confirm the password';
+  }
+
+  if (!Validator.equals(data.password, data.confPassword)) {
+    errors.confPassword = 'Passwords do not match';
   }
 
   return {
