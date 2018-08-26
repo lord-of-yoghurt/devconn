a3029a7407c2cca83756cb34b48977cde1489eb9
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-26 10:05:40 +0800

    create POST /api/profile handler, start filling it out

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,14 +9,14 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route   GET /api/profile/test
-// @desc    tests profile route
+// @desc    test profile route
 // @access  public
 router.get('/test', (req, res) => {
   res.json({ test: 'testProfile' });
 });
 
 // @route   GET /api/profile
-// @desc    get current user's profile
+// @desc    send current user's profile
 // @access  private
 router.get(
   '/', 
@@ -33,6 +33,36 @@ router.get(
         res.json(profile)
       })
       .catch((e) => res.status(404).json(e));
+  }
+);
+
+// @route   POST /api/profile
+// @desc    receive user's profile data and create/edit profile
+// @access  private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // Get the fields that need further processing
+    const { 
+      skills, 
+      youtube, 
+      twitter, 
+      facebook, 
+      linkedin, 
+      instagram 
+    } = req.body;
+
+    // this is all of the fields
+    const profileFields = {
+      // first, spread the req.body object...
+      ...req.body,
+      // then, add the user reference...
+      user: req.user.id,
+      // then, overwrite skills and social properly
+      skills: skills.split(','),
+      social: { youtube, twitter, facebook, linkedin, instagram }
+    };
   }
 );
 
