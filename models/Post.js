91eb23b7888a507e366da1ae8beefa9dfe721168
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-31 09:55:31 +0800

    create Post model

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose'),
+      { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  // this and the avatar will come from the
+  // user instance
+  name: String,
+  avatar: String,
+  // collect objectId's of users who liked a post
+  // removing the like will remove the objectId
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: String,
+      avatar: String,
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('posts', PostSchema);
