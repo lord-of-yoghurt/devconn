6658a132cf4178d1e1206db88bdef36feabc5708
David Ordovskiy <david.ordovsky@gmail.com>
2018-09-01 22:58:22 +0800

    add route to delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Post');
 
 const validatePostInput = require('../../validations/post');
 
@@ -29,7 +30,7 @@ router.get('/', (req, res) => {
 // @desc    find post by id and return
 // @access  public
 router.get('/:id', (req, res) => {
-  Post.findById({ _id: req.params.id })
+  Post.findById(req.params.id)
     .then((post) => {
       if (!post) return res.status(404).json({
         error: 'Post not found'
@@ -61,6 +62,45 @@ router.post(
     newPost.save()
       .then((saved) => res.json(saved))
       .catch((e) => res.status(400).json(e));
+  }
+);
+
+// @route   DELETE /api/posts/:id
+// @desc    find post by id delete it
+// @access  private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.noProfile = 'How are you even posting without a profile?..';
+          return res.status(404).json(errors);
+        }
+
+        Post.findById(req.params.id)
+          .then((post) => {
+            // Does the post belong to this user?
+            if (post.user.toString() !== req.user.id) {
+              errors.notYourPost = 'Users can only delete their own posts!';
+              return res.status(401).json(errors);
+            }
+
+            // Delete the dam thing
+            post.remove().then(() => res.json({ isDeleted: 'yas' }));
+          })
+          .catch((e) => {
+            errors.error = 'Post not found';
+            res.status(404).json(errors);
+          });
+      })
+      .catch((e) => {
+        errors.error = 'There was an error.';
+        res.status(400).json(errors);
+      });
   }
 );
 
