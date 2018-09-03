a39dd34c6e7f142c36438e45c1b5703bc3677e6d
David Ordovskiy <david.ordovsky@gmail.com>
2018-09-03 09:16:08 +0800

    group tests by route

--- tests/users.test.js
+++ tests/users.test.js
@@ -30,91 +30,97 @@ describe('User router', () => {
       });
   });
 
-  it('registers a new user successfully', (done) => {
-    request(app)
-      .post('/api/users/register')
-      .send(registerOpts)
-      .expect(200)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        expect(res.body.name).toEqual(registerOpts.name);
-        done();
-      });
+  describe('/api/users/register', () => {
+    it('registers a new user successfully', (done) => {
+      request(app)
+        .post('/api/users/register')
+        .send(registerOpts)
+        .expect(200)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          expect(res.body.name).toEqual(registerOpts.name);
+          done();
+        });
+    });
+
+    it('returns errors when register validations fail', (done) => {
+      request(app)
+        .post('/api/users/register')
+        .send({})
+        .expect(400)
+        .then((res) => {
+          expect(res.body.name).toEqual('Name cannot be empty');
+          expect(res.body.email).toEqual('Email cannot be empty');
+          done();
+        });
+    });
+
+    it('returns an error when an email already exists', (done) => {
+      request(app)
+        .post('/api/users/register')
+        .send(registerOpts)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.email).toEqual('This email already exists!');
+          done();
+        });
+    });
   });
 
-  it('returns errors when register validations fail', (done) => {
-    request(app)
-      .post('/api/users/register')
-      .send({})
-      .expect(400)
-      .then((res) => {
-        expect(res.body.name).toEqual('Name cannot be empty');
-        expect(res.body.email).toEqual('Email cannot be empty');
-        done();
-      });
+  describe('/api/users/login', () => {
+    it('returns an error if the user doesn\'t exist', (done) => {
+      request(app)
+        .post('/api/users/login')
+        .send(wrongEmail)
+        .expect(404)
+        .then((res) => {
+          expect(res.body.email).toEqual('User not found!');
+          done();
+        });
+    });
+
+    it('returns an error if password is wrong', (done) => {
+      request(app)
+        .post('/api/users/login')
+        .send(wrongPassword)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.password).toEqual('Incorrect email or password');
+          done();
+        });
+    });
+
+    it('logs user in and receives a token', (done) => {
+      request(app)
+        .post('/api/users/login')
+        .send(loginOpts)
+        .expect(200)
+        .then((res) => {
+          token = res.body.token;
+          expect(token).toBeTruthy();
+          done();
+        });
+    });
   });
 
-  it('returns an error when an email already exists', (done) => {
-    request(app)
-      .post('/api/users/register')
-      .send(registerOpts)
-      .expect(400)
-      .then((res) => {
-        expect(res.body.email).toEqual('This email already exists!');
-        done();
-      });
-  });
+  describe('/api/users/current', () => {
+    it('returns Unauthorized with wrong token', (done) => {
+      request(app)
+        .get('/api/users/current')
+        .set('Authorization', 'notatoken')
+        .expect(401, done);
+    });
 
-  it('returns a login error if the user doesn\'t exist', (done) => {
-    request(app)
-      .post('/api/users/login')
-      .send(wrongEmail)
-      .expect(404)
-      .then((res) => {
-        expect(res.body.email).toEqual('User not found!');
-        done();
-      });
-  });
-
-  it('returns a login error when password is wrong', (done) => {
-    request(app)
-      .post('/api/users/login')
-      .send(wrongPassword)
-      .expect(400)
-      .then((res) => {
-        expect(res.body.password).toEqual('Incorrect email or password');
-        done();
-      });
-  });
-
-  it('logs user in and receives a token', (done) => {
-    request(app)
-      .post('/api/users/login')
-      .send(loginOpts)
-      .expect(200)
-      .then((res) => {
-        token = res.body.token;
-        expect(token).toBeTruthy();
-        done();
-      });
-  });
-
-  it('returns Unauthorized with wrong token', (done) => {
-    request(app)
-      .get('/api/users/current')
-      .set('Authorization', 'notatoken')
-      .expect(401, done);
-  });
-
-  it('returns a User instance from /current when logged in', (done) => {
-    request(app)
-      .get('/api/users/current')
-      .set('Authorization', token)
-      .expect(200)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        expect(res.body.name).toEqual('Test User');
-        done();
-      });
+    it('returns a User instance when user is logged in', (done) => {
+      request(app)
+        .get('/api/users/current')
+        .set('Authorization', token)
+        .expect(200)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          expect(res.body.name).toEqual('Test User');
+          done();
+        });
+    });
   });
 });
