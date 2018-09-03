c43a5a29fcb54795c5a8503393947b8234e731b5
David Ordovskiy <david.ordovsky@gmail.com>
2018-09-03 20:13:25 +0800

    expand profile creation test and add profile update test

--- tests/02-profile.test.js
+++ tests/02-profile.test.js
@@ -10,7 +10,7 @@ const {
   seedDb
 } = require('./seeds');
 
-let token;
+let token, profileId;
 
 const LOGIN_URI   = '/api/users/login',
       PROFILE_URI = '/api/profile';
@@ -30,6 +30,26 @@ describe('Profile router', () => {
       .post('/api/profile')
       .set('Authorization', token)
       .send(profileOpts)
-      .expect(200, done);
+      .expect(200)
+      .then((res) => {
+        profileId = res.body._id;
+        expect(res.body.handle).toEqual('testuser9000');
+        done();
+      });
+  });
+
+  it('updates profile if already exists', (done) => {
+    request(app)
+      .post('/api/profile')
+      .set('Authorization', token)
+      .send({ ...profileOpts, status: 'Supreme Tester' })
+      // 200 means no validation fails - i.e. profile is updated
+      .expect(200)
+      .then((res) => {
+        // same id means it's the same profile
+        expect(res.body._id).toEqual(profileId);
+        expect(res.body.handle).toEqual(profileOpts.handle);
+        done();
+      });
   });
 });
